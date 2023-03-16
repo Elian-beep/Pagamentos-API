e7cc27d1de51fb98314595db1f146ca73bc50668
Elian-beep <elianoliver101@gmail.com>
2023-03-16 17:21:31 -0300

    add query and DTO

--- src/main/java/uea/pagamentos_api/repositories/LancamentoRepository.java
+++ src/main/java/uea/pagamentos_api/repositories/LancamentoRepository.java
@@ -3,7 +3,8 @@ package uea.pagamentos_api.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import uea.pagamentos_api.models.Lancamento;
+import uea.pagamentos_api.repositories.lancamento.LancamentoRepositoryQuery;
 
-public interface LancamentoRepository extends JpaRepository<Lancamento, Long> {
+public interface LancamentoRepository extends JpaRepository<Lancamento, Long>, LancamentoRepositoryQuery {
 	
 }
---
+++ src/main/java/uea/pagamentos_api/repositories/lancamento/LancamentoRepositoryQueryImpl.java
@@ -0,0 +1,16 @@
+package uea.pagamentos_api.repositories.lancamento;
+
+import java.util.List;
+
+import uea.pagamentos_api.dto.ResumoLancamentoDto;
+import uea.pagamentos_api.repositories.filters.LancamentoFilter;
+
+public class LancamentoRepositoryQueryImpl implements LancamentoRepositoryQuery {
+
+	@Override
+	public List<ResumoLancamentoDto> filtrar(LancamentoFilter lancamentoFilter) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
